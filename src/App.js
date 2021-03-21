eeffbda6278568a35691bf657ddab17141d88ec3
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-21 17:12:44 -0300

    Added a profile form to the App component. Added a button to preview the CV.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Profile">
+          <form>
+            <label htmlFor="first-name">First name:</label>
+            <input type="text" id="first-name" name="first-name"></input>
+            <label htmlFor="last-name">Last name:</label>
+            <input type="text" id="last-name" name="last-name"></input>
+            <label htmlFor="profession">Profession:</label>
+            <input type="text" id="profession" name="profession"></input>
+            <label htmlFor="city">City:</label>
+            <input type="text" id="city" name="citye"></input>
+            <label htmlFor="country">Contry:</label>
+            <input type="text" id="country" name="country"></input>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email"></input>
+          </form>
+          <button type="button">Preview</button>
+        </div>
+        {/*<div className="Work">
+          <form></form>
+        </div>
+        <div className="Education">
+          <form></form>
+    </div>*/}
+      </div>
+    );
+  }
 }
+
+/*class Education extends Component {
+  render() {
+    return (
+      <form>
+        <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname">
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname">
+  <input type="submit" value="Submit"></input>
+      </form>
+    )
+  }
+}*/
 
 export default App;
