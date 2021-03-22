bf209a2e8085fdc375318fc4ad0a1cae8c77b3e3
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-22 14:52:36 -0300

    Added a languages section to the left section.

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,5 +1,26 @@
 function Languages(props) {
-  return <section className="Languages"></section>;
+  if (props.isPreviewMode === true) {
+    return <languagesPreview languages={props.languages} />;
+  } else {
+    return <LanguagesInput languages={props.languages} />;
+  }
+}
+
+function LanguagesInput(props) {
+  //const skills = props.skills;
+
+  return (
+    <section className="LanguagesSection">
+      <h5>LANGUAGES</h5>
+      <button type="button" className="langauges-button">
+        <i class="fas fa-plus"></i>
+      </button>
+    </section>
+  );
+}
+
+function languagesPreview(props) {
+  return;
 }
 
 export default Languages;
